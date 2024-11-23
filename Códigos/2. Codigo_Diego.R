276e1bd8055e7009fb60a114c91d6852abf8d104
Diego Fernando Cuesta Mora <dfcuesta99@gmail.com>
2024-11-23 14:48:58 -0500

    Update 2. Codigo_Diego.R

--- Códigos/2. Codigo_Diego.R
+++ Códigos/2. Codigo_Diego.R
@@ -2,3 +2,132 @@
 #----------------------------- Pruebas - Diego  -------------------------------#
 #------------------------------------------------------------------------------#
 
+wd <- "C:\\Users\\HP\\OneDrive - Universidad Nacional de Colombia\\Documentos\\Diego\\PEG\\2024-2\\Machine learning\\Repositorios\\Problem-Set-3-Machine-Learning_2024\\Bases"
+setwd(wd)
+
+# 1. Cargue base de datos ------------------------------------------------------
+Train <- readRDS("train.rds")
+Test  <- readRDS("test.rds")
+
+db_miss_train <- skim(Train)%>% dplyr::select(skim_variable, n_missing)
+db_miss_test  <- skim(Test)%>% dplyr::select(skim_variable, n_missing)
+
+# 1.1. Variables de interes ----------------------------------------------------
+train <- Train %>% 
+  select(-surface_total, -surface_covered, -rooms, -bedrooms, -bathrooms)
+
+test <- Test %>% 
+  select(-surface_total, -surface_covered, -rooms, -bedrooms, -bathrooms)
+
+# 2. Arreglo de datos ----------------------------------------------------------
+
+# Se guardan las descripciones en un vector source
+descriptions_train <- train$description
+des_train_scource <- VectorSource(descriptions_train)
+descriptions_test <- test$description
+des_test_scource <- VectorSource(descriptions_test)
+
+# Make a volatile corpus: coffee_corpus
+des_corpus_train <- VCorpus(des_train_scource, readerControl = list( language = "es"))
+des_corpus_test <- VCorpus(des_test_scource, readerControl = list( language = "es"))
+
+# Función para reemplazar números por palabras
+reemplazar_numeros <- function(texto) {
+  palabras <- c("cero", "uno", "dos", "tres", "cuatro", "cinco", "seis", "siete", "ocho", "nueve", "diez")
+  # Reemplazar números del 0 al 10 por palabras
+  for (i in 0:10) {
+    texto <- gsub(sprintf("\\b%d\\b", i), palabras[i + 1], texto)}
+  return(texto)}
+
+# Convertir texto a formato ASCII eliminando tildes y caracteres especiales
+eliminar_tildes <- function(texto) {
+  texto_sin_tildes <- iconv(texto, "UTF-8", "ASCII", sub = "")
+  return(texto_sin_tildes)}
+
+reemplazar_car_especiales <- function(texto) {
+  texto_sin_espe <-str_replace_all(texto, "[^[:alnum:]]", " ")
+  return(texto_sin_espe)}
+
+## volver a las palabras a sus raíces
+stem_espanol<-  function(texto) {
+  texto_stem <- stemDocument(texto, language="spanish")
+  return(texto_stem)}
+
+# Descargamos la lista de las stopwords en español de dos fuentes diferentes y las combinamos
+lista_palabras1 <- stopwords(language = "es", source = "snowball")
+lista_palabras2 <- stopwords(language = "es", source = "nltk")
+lista_palabras <- union(lista_palabras1, lista_palabras2)
+lista_palabras<- union(lista_palabras,  c("vendo", "venta", "vende", "etc", "carrera", "calle", "casa", "apto", "apartamento",
+                                          "ubicado","ubicada") )
+
+clean_corpus <- function(corpus){
+  corpus <- tm_map(corpus, stripWhitespace) ## remover espacios en blanco
+  corpus <- tm_map(corpus, removePunctuation)  ## remover puntuacióm
+  corpus <- tm_map(corpus, content_transformer(tolower)) # todo minuscula 
+  corpus <- tm_map(corpus, removeWords, c(lista_palabras)) # remover stopwords y otras que se quieran aádir
+  corpus<-  tm_map(corpus, content_transformer(reemplazar_numeros))  ## incluir funciones que nosotros creamos 
+  corpus<-  tm_map(corpus, content_transformer(eliminar_tildes))  ## incluir funciones que nosotros creamos
+  corpus<-  tm_map(corpus, content_transformer(reemplazar_car_especiales))  ## incluir funciones que nosotros creamos
+  corpus<-  tm_map(corpus, content_transformer(stem_espanol))
+  corpus<-  tm_map(corpus, removeNumbers)  # remover numeros restantes
+  return(corpus)}
+
+# apliquemos nuestra función de limpieza:
+clean_des_train <- clean_corpus(des_corpus_train)
+clean_des_test <- clean_corpus(des_corpus_test)
+
+# crear la document - term matrix
+description_dtm_train <- DocumentTermMatrix(clean_des_train)
+description_dtm_test <- DocumentTermMatrix(clean_des_test)
+
+# dejar en train solo variables que comparta con test
+des_train <- as.data.frame(as.matrix(removeSparseTerms(description_dtm_train, 0.9), sparse=TRUE))
+des_test <- as.data.frame(as.matrix(removeSparseTerms(description_dtm_test, 0.9), sparse=TRUE))
+
+var_compartidas <- intersect(names(des_train), names(des_test))
+des_train <- des_train[,var_compartidas]
+des_test <- des_test[,var_compartidas]
+
+# componentes principales eliminando las q tienen 90% de entradas nulas
+pcdescriptions_train <- prcomp(as.matrix(des_train), scale=TRUE)
+pcdescriptions_test <- prcomp(as.matrix(des_test), scale=TRUE)
+
+# guardar componentes 
+zdes_train <- as.data.frame(predict(pcdescriptions_train)) %>%
+  mutate(property_id = train$property_id)
+
+zdes_test <- as.data.frame(predict(pcdescriptions_test)) %>%
+  mutate(property_id = test$property_id)
+
+des_train <- des_train %>%
+  mutate(property_id = train$property_id)
+
+des_test <- des_test %>%
+  mutate(property_id = test$property_id)
+
+# unir bases de datos de texto y de componentes principales
+train_full<-  train %>% 
+  full_join(des_train, join_by(property_id)) %>%
+  full_join(zdes_train, join_by(property_id))
+
+test_full<-  test %>% 
+  full_join(des_test, join_by(property_id)) %>%
+  full_join(zdes_test, join_by(property_id))
+
+# 3. Boosting ------------------------------------------------------------------
+
+## Creamos una data full con las dummys 
+# Crear dummys train
+dummys <- dummy(subset(train_full, select = c(property_type_2, barrio)))
+dummys <- as.data.frame(apply(dummys,2,function(x){as.numeric(x)}))
+train_full_dummys <- cbind(subset(train_full, select = -c(property_type, localidad)),dummys)
+
+#crear dummys test
+dummys <- dummy(subset(test_full, select = c(property_type_2, barrio)))
+dummys <- as.data.frame(apply(dummys,2,function(x){as.numeric(x)}))
+test_full_dummys <- cbind(subset(test_full, select = -c(property_type, localidad)),dummys)
+
+#dejar variables que comparten test y train despues de crear dummys
+train_full_dummys <- train_full_dummys[c(colnames(test_full_dummys),"price")]
+#Quitamos el segundo price de la train full dummys
+train_full_dummys$price.1=NULL
