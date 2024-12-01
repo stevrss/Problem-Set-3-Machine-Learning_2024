a7bfbfca6895091f9a17d52084b8bf329c968ccb
Diego Fernando Cuesta Mora <dfcuesta99@gmail.com>
2024-12-01 13:31:43 -0500

    Update 2. Codigo_Diego.R

--- Códigos/2. Codigo_Diego.R
+++ Códigos/2. Codigo_Diego.R
@@ -183,17 +183,25 @@ fitControl <- trainControl(
 
 #Cargamos los parámetros del boosting
 grid_xbgoost <- expand.grid(nrounds = c(500),
-                            max_depth = c(4,6), 
-                            eta = c(0.05,0.1,0.15,0.2,0.25), 
-                            gamma = c(0.1,0.2), 
-                            min_child_weight = c(10,15,20,25),
+                            max_depth = c(6), 
+                            eta = c(0.05,0.1,0.15), 
+                            gamma = c(0.1,0.15,0.2,0.25), 
+                            min_child_weight = c(10,15,20),
                             colsample_bytree = c(0.66),
                             subsample = c(0.8))
 
 set.seed(1536)
 
+# Se paraleliza
+library(doParallel)
+num_cores <- parallel::detectCores()
+print(num_cores)
+cl <- makeCluster(9)  # Usar 6
+registerDoParallel(cl)
+#stopCluster(cl)    # Para liberar los nucleos
+
 library(caret)
-XGBoost_model_1 <- caret::train(price ~ distancia_parque + area_parque + distancia_policia + distancia_gym +
+XGBoost_model_1 <- caret::train(price ~ month+ year + distancia_parque + area_parque + distancia_policia + distancia_gym +
                           distancia_bus + distancia_super + distancia_bar + distancia_hosp + 
                           distancia_cole + distancia_cc + distancia_rest + distancia_libreria + 
                           distancia_uni + distancia_banco + dist_avenida + rooms_imp2 + bedrooms + 
@@ -205,14 +213,17 @@ XGBoost_model_1 <- caret::train(price ~ distancia_parque + area_parque + distanc
                           integral + interior + lavanderi + lind + mader + mts + natural + parqu + parqueader + pis +
                           principal + priv + remodel + rop + sal + salon + sector + segur + servici + social + terraz + 
                           tres + ubicacion + uno + vias + vigil + visit + vist + zon + PC1 + PC2 + PC3 + PC4 + PC5 + PC6 + 
-                          PC7 + PC8 + PC9 + PC10 + PC11 + PC12 + PC13 + PC14 + PC15 + property_type_2_Apartamento + 
+                          PC7 + PC8 + PC9 + PC10 + PC11 + PC12 + PC13 + PC14 + PC15 + PC16 + PC17 + PC18 + PC19 + PC20 + PC21 + 
+                          PC22 + PC23 + PC24 + PC25 + PC26 + PC27 + PC28 + PC29 + PC30 + PC31 + PC32 + PC33 + PC34 + PC35 + PC36 +
+                          PC37 + PC38 + PC39 + PC40 + PC41 + PC42 + PC43 + PC44 + PC45 + property_type_2_Apartamento + 
                           surface_total_median2^2 + rooms_imp2^2 +
                           surface_covered_median2^2+bedrooms^2+localidad_BARRIOS.UNIDOS + localidad_CANDELARIA + localidad_CHAPINERO +
                           localidad_ENGATIVA + localidad_PUENTE.ARANDA + localidad_SANTA.FE + localidad_SUBA + 
                           localidad_TEUSAQUILLO + localidad_USAQUEN+estrato_imp +zona_g_t+bathrooms_imp2/surface_total_median2+
                           bedrooms/surface_total_median2+rooms_imp2/surface_total_median2+property_type_2_Apartamento*ascensor+
-                          bathrooms_imp2/rooms_imp2+bathrooms_imp2/bedrooms,
-                          data=train2[-1], #excluye variable de property_id
+                          bathrooms_imp2/rooms_imp2+bathrooms_imp2/bedrooms+distancia_parque*property_type_2_Apartamento+distancia_cc^2+ distancia_super^2+
+                          distancia_policia^2+distancia_gym^2+distancia_bus^2+distancia_hosp^2+distancia_libreria^2+distancia_uni^2+dist_avenida^2+parqueader*property_type_2_Apartamento
+                          ,data=train2[-1], #excluye variable de property_id
                           method = "xgbTree",
                           trControl = fitControl,
                           metric = "MAE", # Indica que la métrica objetivo es MAE
@@ -240,14 +251,14 @@ train_XGBoost_model_1 <- train2 %>%
   mutate(price_pred = predict(XGBoost_model_1, newdata = train2))  
 
 mae_value_train <- mean(abs(train_XGBoost_model_1$price - train_XGBoost_model_1$price_pred))
-print(mae_value_train)  # 89613366 vs 70664313 vs 70236645 vs 60465280 vs 55911592
+print(mae_value_train)  # 89613366 vs 70664313 vs 70236645 vs 60465280 vs 48780882 VF
 
 # Prediccion fuera de muestra 
 test_XGBoost_model_1 <- test2 %>% 
   mutate(price_pred = predict(XGBoost_model_1, newdata = test2))  
 
 mae_value_test <- mean(abs(test_XGBoost_model_1$price - test_XGBoost_model_1$price_pred))
-print(mae_value_test)  # 117231096 vs 109446775 vs 109021105 vs 106177795 vs 106437327
+print(mae_value_test)  # 117231096 vs 109446775 vs 109021105 vs 106177795 vs 106437327 vs 106804677 VF
 
 # Importancia de variables 
 p_load(DiagrammeR)
@@ -271,73 +282,137 @@ write.csv(test_XGBoost_model_1,"XGBoost_model1_nr500_maxd6_eta0.15_g0.2_col0.66_
 
 # 3.2  XGbosst 1 - Validacion cruzada espacial ---------------------------------
 
+# Configuaracion del modelo 
+xgboost_spec <- boost_tree(
+  trees = tune(),            # Número de árboles
+  tree_depth = tune(),       # Profundidad del árbol
+  learn_rate = tune(),       # Tasa de aprendizaje
+  loss_reduction = tune(),   # Gamma (reducción mínima de pérdida)
+  sample_size = tune(),      # Submuestreo de observaciones
+  mtry = tune(),             # Número de predictores seleccionados
+  min_n = tune()             # Mínimo de observaciones por hoja
+) %>%
+  set_engine("xgboost") %>%  # Especificar el motor
+  set_mode("regression")     # Modo de regresión
+
+# Definir un grid para los hiperparámetros
+xgb_grid <- expand.grid(
+  trees = c(500),
+  tree_depth = c(6),
+  learn_rate = c(0.1),
+  loss_reduction = c(0.1),
+  sample_size = c(0.8),
+  mtry = c(140),
+  min_n = c(15)
+)
+
+#xgb_grid <- expand.grid(
+#  trees = 500,
+#  tree_depth = c(4, 6),
+#  learn_rate = c(0.05, 0.1, 0.15, 0.2),
+#  loss_reduction = c(0.1, 0.2, 0.3),
+#  sample_size = c(0.6, 0.8),
+#  mtry = 136,
+#  min_n = c(10, 15, 20, 25)
+# )
 
 
+# Primera receta
+rec_1 <- recipe(price ~ distancia_parque + area_parque + distancia_policia + distancia_gym +
+                  distancia_bus + distancia_super + distancia_bar + distancia_hosp + 
+                  distancia_cole + distancia_cc + distancia_rest + distancia_libreria + 
+                  distancia_uni + distancia_banco + dist_avenida + rooms_imp2 + bedrooms + 
+                  bathrooms_imp2 + surface_total_median2 + surface_covered_median2 + abiert + acab + acces + alcob + 
+                  ampli + are + ascensor + balcon + ban + bao + baos + bbq + bogot + buen + centr +
+                  cerc + cerr + chimene + closet + cocin + comedor + comercial + comunal + cuart + 
+                  cuatr + cubiert + cuent + deposit + dos + edifici + espaci + estudi + excelent + 
+                  exterior + garaj + gas + gimnasi + habit + habitacion + hermos + ilumin + independient + 
+                  integral + interior + lavanderi + lind + mader + mts + natural + parqu + parqueader + pis +
+                  principal + priv + remodel + rop + sal + salon + sector + segur + servici + social + terraz + 
+                  tres + ubicacion + uno + vias + vigil + visit + vist + zon + PC1 + PC2 + PC3 + PC4 + PC5 + PC6 + 
+                  PC7 + PC8 + PC9 + PC10 + PC11 + PC12 + PC13 + PC14 + PC15 + property_type_2_Apartamento +
+                  localidad_BARRIOS.UNIDOS + localidad_CANDELARIA + localidad_CHAPINERO +
+                  localidad_ENGATIVA + localidad_PUENTE.ARANDA + localidad_SANTA.FE + localidad_SUBA + 
+                  localidad_TEUSAQUILLO + localidad_USAQUEN+estrato_imp +zona_g_t + barrio
+                , data = train2[-1]) %>%
+  step_interact(terms = ~ bathrooms_imp2:surface_total_median2+
+                  bedrooms:surface_total_median2+rooms_imp2:surface_total_median2+property_type_2_Apartamento:ascensor+
+                  bathrooms_imp2:rooms_imp2+bathrooms_imp2:bedrooms) %>% 
+  step_poly(surface_total_median2, surface_covered_median2,rooms_imp2,bedrooms,distancia_policia,
+            distancia_libreria, distancia_gym , distancia_bus , distancia_super , distancia_bar , distancia_hosp , 
+              distancia_cole , distancia_cc , distancia_rest , distancia_libreria , 
+              distancia_uni , distancia_banco , dist_avenida, degree = 2) %>%
+  step_novel(all_nominal_predictors()) %>% 
+  step_dummy(all_nominal_predictors()) %>% 
+  step_zv(all_predictors()) %>% 
+  step_normalize(all_predictors())
 
+# Iniciar un flujo de trabajo utilizando 'workflow()'
+workflow_1 <- workflow() %>% 
+  # Agregar la receta de preprocesamiento de datos. En este caso la receta 1
+  add_recipe(rec_1) %>%
+  # Agregar la especificación del modelo xgboost
+  add_model(xgboost_spec)
 
+# Validacion cruzada espacial 
+# definimos nuestra variable como sf
+train_sf <- st_as_sf(
+  train2[-1],
+  # "coords" is in x/y order -- so longitude goes first!
+  coords = c("lon", "lat"),
+  # Set our coordinate reference system to EPSG:4326,
+  # the standard WGS84 geodetic coordinate reference system
+  crs = 4326
+)
 
+# Proceso de validacion 
+set.seed(1536)
+block_folds <- spatial_block_cv(train_sf, v = 5)
+autoplot(block_folds)
 
+# Se paraleliza
+#library(doParallel)
+#num_cores <- parallel::detectCores()
+#print(num_cores)
+#cl <- makeCluster(6)  # Usar 6
+#registerDoParallel(cl)
+#stopCluster(cl)    # Para liberar los nucleos
 
+# Vamos a encontrar los mejores hiperparámetros 
+set.seed(1536)
 
+tune_res1 <- tune_grid(
+  workflow_1,               # El flujo de trabajo que contiene: receta y especificación del modelo
+  resamples = block_folds,  # Folds de validación cruzada espacial
+  grid = xgb_grid,          # Grilla de valores
+  metrics = metric_set(mae) # métrica
+)
 
+# Ver los resultados 
+collect_metrics(tune_res1)
 
+# Utilizar 'select_best' para seleccionar el mejor valor.
+best_tune_res1 <- select_best(tune_res1, metric = "mae")
+best_tune_res1
 
+# Finalizar el flujo de trabajo 'workflow' con el mejor valor de parametros
+res1_final <- finalize_workflow(workflow_1, best_tune_res1)
 
+# Ajustar el modelo  utilizando los datos de entrenamiento
+EN_final1_fit <- fit(res1_final, data = train2[-1])
 
+# Ver resultados en test2 
+augment(EN_final1_fit, new_data = test2) %>%
+  mae(truth = price, estimate = .pred)
 
+# Ver resultados en test original
+test_XGBoost_model_1_vce = augment(EN_final1_fit, new_data = test_full_dummys)
 
+test_XGBoost_model_1 <- test_XGBoost_model_1_vce %>%
+  mutate(price = .pred) %>% 
+  select(property_id,price) 
 
-
-
-
-
-
-
-
-
-
-
-
-Formula_1_XG = as.formula("price ~ distancia_parque + area_parque + distancia_policia + distancia_gym +
-                          distancia_bus + distancia_super + distancia_bar + distancia_hosp + 
-                          distancia_cole + distancia_cc + distancia_rest + distancia_libreria + 
-                          distancia_uni + distancia_banco + dist_avenida + property_type_2 + rooms_imp2 +
-                          bathrooms_imp2 + bedrooms_imp2 + surface_total_imp_mean2 + surface_covered_imp_mean2 +
-                          surface_total_median2 + surface_covered_median2 + abiert + acab + acces + alcob + 
-                          ampli + are + ascensor + balcon + ban + bao + baos + bbq + bogot + buen + centr +
-                          cerc + cerr + chimene + closet + cocin + comedor + comercial + comunal + cuart + 
-                          cuatr + cubiert + cuent + deposit + dos + edifici + espaci + estudi + excelent + 
-                          exterior + garaj + gas + gimnasi + habit + habitacion + hermos + ilumin + independient + 
-                          integral + interior + lavanderi + lind + mader + mts + natural + parqu + parqueader + pis +
-                          principal + priv + remodel + rop + sal + salon + sector + segur + servici + social + terraz + 
-                          tres + ubicacion + uno + vias + vigil + visit + vist + zon + PC1 + PC2 + PC3 + PC4 + PC5 + PC6 + 
-                          PC7 + PC8 + PC9 + PC10 + PC11 + PC12 + PC13 + PC14 + PC15 + PC16 + PC17 + PC18 + PC19 + PC20 + 
-                          PC21 + PC22 + PC23 + PC24 + PC25 + PC26 + PC27 + PC28 + PC29 + PC30 + PC31 + PC32 + PC33 + PC34 + 
-                          PC35 + PC36 + PC37 + PC38 + PC39 + PC40 + PC41 + PC42 + PC43 + PC44 + PC45 + PC46 + PC47 + PC48 + 
-                          PC49 + PC50 + PC51 + PC52 + PC53 + PC54 + PC55 + PC56 + PC57 + PC58 + PC59 + PC60 + PC61 + PC62 + 
-                          PC63 + PC64 + PC65 + PC66 + PC67 + PC68 + PC69 + PC70 + PC71 + property_type_2_Apartamento + 
-                          property_type_2_Casa + n_pisos_numerico + piso_numerico + surface_covered_imp_mean2^2 + rooms_imp2^2 +
-                          surface_total_imp_mean2^2")
-
-train(price ~ distancia_parque + area_parque + distancia_policia + distancia_gym +
-        distancia_bus + distancia_super + distancia_bar + distancia_hosp + 
-        distancia_cole + distancia_cc + distancia_rest + distancia_libreria + 
-        distancia_uni + distancia_banco + dist_avenida + rooms_imp2 + bedrooms + 
-        bathrooms_imp2 + surface_total_median2 + surface_covered_median2 + abiert + acab + acces + alcob + 
-        ampli + are + ascensor + balcon + ban + bao + baos + bbq + bogot + buen + centr +
-        cerc + cerr + chimene + closet + cocin + comedor + comercial + comunal + cuart + 
-        cuatr + cubiert + cuent + deposit + dos + edifici + espaci + estudi + excelent + 
-        exterior + garaj + gas + gimnasi + habit + habitacion + hermos + ilumin + independient + 
-        integral + interior + lavanderi + lind + mader + mts + natural + parqu + parqueader + pis +
-        principal + priv + remodel + rop + sal + salon + sector + segur + servici + social + terraz + 
-        tres + ubicacion + uno + vias + vigil + visit + vist + zon + PC1 + PC2 + PC3 + PC4 + PC5 + PC6 + 
-        PC7 + PC8 + PC9 + PC10 + PC11 + PC12 + PC13 + PC14 + PC15 + property_type_2_Apartamento + 
-        property_type_2_Casa + n_pisos_numerico + piso_numerico + surface_total_median2^2 + rooms_imp2^2 +
-        surface_covered_median2^2+bedrooms^2+localidad_BARRIOS.UNIDOS + localidad_CANDELARIA + localidad_CHAPINERO +
-        localidad_ENGATIVA + localidad_PUENTE.ARANDA + localidad_SANTA.FE + localidad_SUBA + 
-        localidad_TEUSAQUILLO + localidad_USAQUEN+estrato_imp +zona_g_t+bathrooms_imp2/surface_total_median2+
-        bedrooms/surface_total_median2+rooms_imp2/surface_total_median2+property_type_2_Apartamento*ascensor+
-        bathrooms_imp2/rooms_imp)
-
-#
-      
+# Guardar prediccion
+setwd(paste0(wd,"\\Resultados\\XGboost"))
+write.csv(test_XGBoost_model_1,"XGBoost_model_espacial_nr500_maxd6_eta0.15_g0.2_col0.66_min10_sub0.8.csv",row.names = F) 
+#Puntaje Kaggle: 230537671.28 vs 233586592.35 vs 227872992.63 vs 228451088.75 vs 
